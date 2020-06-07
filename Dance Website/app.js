9abc4c30b524e8be56267c691ac592eaf6bb3f1d
Shrishty Dayal <500068672@stu.upes.ac.in>
2020-06-07 17:56:50 +0530

    Update app.js
    I have used mongoose for the Node.js  and MongoDB as it allows us to have access to the MongoDB commands for easily. To use mongoose, make sure that you add it to you Node project and mongoose schema and model are created .

--- Dance Website/app.js
+++ Dance Website/app.js
@@ -1,29 +1,54 @@
 const express= require('express');
 const path = require('path');
-// const fs= require('fs') as not used this time
+
 const app = express();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/contactdance', {useNewUrlParser: true});
 const port =80;
+
+//we have used mongoose schema...(similar to mysql)
+ //define mongoose schema
+ var contactSchema = new mongoose.Schema({
+    name: String,
+    age: String,
+    email: String,
+    address:String,
+    phone: String
+   
+ });
+  var Contact = mongoose.model('Contact', contactSchema);
 
 // EXPRESS SPECIFIC STUFF
 
-app.use('/static', express.static('static')) // For serving static files
-app.use(express.urlencoded())
-// PUG SPECIFIC STUFF
+ app.use('/static', express.static('static')) // For serving static files
+ app.use(express.urlencoded())
+ // PUG SPECIFIC STUFF
 app.set('view engine', 'pug') // Set the template engine as pug
 app.set('views', path.join(__dirname, 'views')) // Set the views directory
  
-app.get('/', (req, res)=>{
+ app.get('/', (req, res)=>{
    
-    const params = { }
-    res.status(200).render('home.pug', params);
-})
-app.get('/contact', (req, res)=>{
+     const params = { }
+     res.status(200).render('home.pug', params);
+ })
+ app.get('/contact', (req, res)=>{
    
-    const params = { }
-    res.status(200).render('contact.pug', params);
+     const params = { }
+     res.status(200).render('contact.pug',params);
+ })
+ app.post('/contact', (req, res)=>{
+    var myData = new Contact(req.body);
+    myData.save().then(()=>{  
+       res.send("this item has been saved to the database")   
+
+    }).catch(()=>{
+        res.status(400).send("item was not saved to the database")
+   });
 })
 
+
 // START THE SERVER
-app.listen(port, ()=>{
+ app.listen(port, ()=>{
     console.log(`The application started successfully on port ${port}`);
-});
+ }); 
+
